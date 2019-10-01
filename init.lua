37d8d86efb8ece9ea333a1c3ef19cdfa1a852822
TheTermos <55103816+TheTermos@users.noreply.github.com>
2019-10-01 11:47:12 +0200

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,264 @@
+local floor = math.floor
+local ceil = math.ceil
+local min = math.min
+local max = math.max
+local random = math.random
+
+local convex = false
+local mpath = minetest.get_modpath('islands')
+
+dofile(mpath ..'\\registerstuff.lua')
+
+local mult = 1.0
+-- Set the 3D noise parameters for the terrain.
+
+
+local np_terrain = {
+--	offset = -13,
+	offset = -11*mult,						-- ratio 2:7 or 1:4 ?
+	scale = 40*mult,
+--	scale = 30,
+	spread = {x = 256*mult, y =256*mult, z = 256*mult},
+--	spread = {x = 128, y =128, z = 128},
+	seed = 1234,
+	octaves = convex and 1 or 5,
+	persist = 0.38,
+	lacunarity = 2.33,
+	--flags = "eased"
+}	--]]
+
+local np_var = {
+	offset = 0,						
+	scale = 6*mult,
+	spread = {x = 64*mult, y =64*mult, z = 64*mult},
+	seed = 567891,
+	octaves = 4,
+	persist = 0.4,
+	lacunarity = 1.89,
+	--flags = "eased"
+}
+
+local np_hills = {
+	offset = 2.5,					-- off/scale ~ 2:3
+	scale = -3.5,
+	spread = {x = 64*mult, y =64*mult, z = 64*mult},
+--	spread = {x = 32, y =32, z = 32},
+	seed = 2345,
+	octaves = 3,
+	persist = 0.40,
+	lacunarity = 2.0,
+	flags = "absvalue"
+}
+
+local np_cliffs = {
+	offset = 0,					
+	scale = 0.72,
+	spread = {x = 180*mult, y =180*mult, z = 180*mult},
+	seed = 78901,
+	octaves = 2,
+	persist = 0.4,
+	lacunarity = 2.11,
+--	flags = "absvalue"
+}
+
+local hills_offset = np_hills.spread.x*0.5
+local hills_thresh = floor((np_terrain.scale)*0.5)
+local shelf_thresh = floor((np_terrain.scale)*0.5) 
+local cliffs_thresh=10
+
+local function max_height(noiseprm)
+	local height = 0
+	local scale = noiseprm.scale
+	for i=1,noiseprm.octaves do
+		height=height + scale
+		scale = scale * noiseprm.persist
+	end	
+	return height+noiseprm.offset
+end
+
+local function min_height(noiseprm)
+	local height = 0
+	local scale = noiseprm.scale
+	for i=1,noiseprm.octaves do
+		height=height - scale
+		scale = scale * noiseprm.persist
+	end	
+	return height+noiseprm.offset
+end
+
+local base_min = min_height(np_terrain)
+local base_max = max_height(np_terrain)
+local base_rng = base_max-base_min
+local easing_factor = 1/(base_max*base_max*4)
+local base_heightmap = {}
+
+
+-- Set singlenode mapgen (air nodes only).
+-- Disable the engine lighting calculation since that will be done for a
+-- mapchunk of air nodes and will be incorrect after we place nodes.
+
+--minetest.set_mapgen_params({mgname = "singlenode", flags = "nolight"})
+
+minetest.set_mapgen_setting('mgname','singlenode',true)
+minetest.set_mapgen_setting('flags','nolight',true)
+
+
+-- Get the content IDs for the nodes used.
+
+local c_sandstone = minetest.get_content_id("default:sandstone")
+local c_stone = minetest.get_content_id("default:stone")
+local c_sand = minetest.get_content_id("default:sand")
+--local c_dirt = minetest.get_content_id("default:dirt_with_grass")
+--local c_dirt = minetest.get_content_id("default:dirt_with_dry_grass")
+local c_dirt = minetest.get_content_id("islands:dirt_with_grass_palm")
+local c_snow = minetest.get_content_id("islands:dirt_with_snow")
+local c_water     = minetest.get_content_id("default:water_source")
+
+
+-- Initialize noise object to nil. It will be created once only during the
+-- generation of the first mapchunk, to minimise memory use.
+
+local nobj_terrain = nil
+local nobj_var = nil
+local nobj_hills = nil
+local nobj_cliffs = nil
+
+
+-- Localise noise buffer table outside the loop, to be re-used for all
+-- mapchunks, therefore minimising memory use.
+
+local nvals_terrain = {}
+local isln_terrain = nil
+local isln_var = nil
+local isln_hills = nil
+local isln_cliffs = nil
+
+
+-- Localise data buffer table outside the loop, to be re-used for all
+-- mapchunks, therefore minimising memory use.
+
+local data = {}
+
+
+-- On generated function.
+
+-- 'minp' and 'maxp' are the minimum and maximum positions of the mapchunk that
+-- define the 3D volume.
+minetest.register_on_generated(function(minp, maxp, seed)
+	-- Start time of mapchunk generation.
+	local t0 = os.clock()
+	
+	local sidelen = maxp.x - minp.x + 1
+--	local permapdims3d = {x = sidelen, y = sidelen, z = sidelen}
+	local permapdims3d = {x = sidelen, y = sidelen, z = 0}
+	
+	-- base terrain
+	nobj_terrain = nobj_terrain or
+		minetest.get_perlin_map(np_terrain, permapdims3d)		
+	isln_terrain=nobj_terrain:get_2d_map({x=minp.x,y=minp.z})
+	
+	-- base variation
+	nobj_var = nobj_var or
+		minetest.get_perlin_map(np_var, permapdims3d)		
+	isln_var=nobj_var:get_2d_map({x=minp.x,y=minp.z})
+	
+	-- hills
+	nobj_hills = nobj_hills or
+		minetest.get_perlin_map(np_hills, permapdims3d)
+	isln_hills=nobj_hills:get_2d_map({x=minp.x+hills_offset,y=minp.z+hills_offset})
+	
+	-- cliffs
+	nobj_cliffs = nobj_cliffs or
+		minetest.get_perlin_map(np_cliffs, permapdims3d)
+	isln_cliffs=nobj_cliffs:get_2d_map({x=minp.x,y=minp.z})
+	
+	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
+	vm:get_data(data)
+
+	for z = minp.z, maxp.z do
+		base_heightmap[z-minp.z+1]={}
+		for x = minp.x, maxp.x do
+			local theight = isln_terrain[z-minp.z+1][x-minp.x+1] + (convex and isln_var[z-minp.z+1][x-minp.x+1] or 0)
+			-- parabolic gradient
+			if theight > 0 and theight < shelf_thresh then
+				theight = theight * (theight*theight/(shelf_thresh*shelf_thresh)*0.5 + 0.5)
+			end	
+			base_heightmap[z-minp.z+1][x-minp.x+1]=theight
+		end
+	end	
+
+	for z = minp.z, maxp.z do
+		for y = minp.y, maxp.y do
+			for x = minp.x, maxp.x do
+				local vi = area:index(x, y, z)								
+				local theight = base_heightmap[z-minp.z+1][x-minp.x+1]
+				
+				-- hills
+				if theight > hills_thresh then
+					theight = theight + max((theight-hills_thresh) * isln_hills[z-minp.z+1][x-minp.x+1],0)
+				-- cliffs
+				elseif theight > 1 and theight < hills_thresh then 
+					local clifh = max(min(isln_cliffs[z-minp.z+1][x-minp.x+1],1),0) 
+					if clifh > 0 then
+						clifh = -1*(clifh-1)*(clifh-1) + 1
+						theight = theight + (hills_thresh-theight) * clifh * ((theight<2) and theight-1 or 1)
+					end
+				end
+				
+				if theight > y then
+					data[vi] = c_stone
+				elseif y==ceil(theight) then
+					data[vi]= y<3 and c_sand or (y<60-random(3) and c_dirt or c_snow)
+				elseif y <= 1 then
+					data[vi] = c_water
+				end
+			end
+		end
+	end
+
+	vm:set_data(data)
+	minetest.generate_decorations(vm)
+	vm:calc_lighting()
+	vm:write_to_map()
+	vm:update_liquids()
+	
+	-- Print generation time of this mapchunk.
+	local chugent = ceil((os.clock() - t0) * 1000)
+	print ("[lvm_example] Mapchunk generation time " .. chugent .. " ms")
+end)
+
+minetest.register_on_chat_message(
+	function(name, message)
+		if message == 'doit' then
+			local plyr = minetest.get_player_by_name('singleplayer')
+			local pos = plyr:get_pos()
+			minetest.chat_send_all(minetest.get_biome_name(minetest.get_biome_data(pos).biome))
+		end
+	end
+)	
+
+
+minetest.register_on_newplayer(function(obj)
+	local nobj_terrain = minetest.get_perlin_map(np_terrain, {x=80,y=80,z=0})	
+	local map=nobj_terrain:get_2d_map({x=-40,y=-40})
+	local height = map[20][20]
+
+	minetest.set_timeofday(0.35)
+	local inv = obj:get_inventory()
+	inv:add_item('main','binoculars:binoculars')
+	local pos = obj:get_pos()
+	local node = minetest.get_node(pos)
+	if height<1 then
+		pos.y = 2
+		minetest.add_entity(pos,'boats:boat')
+		pos.y = 3
+		obj:set_pos(pos)
+	else
+		inv:add_item("main", "boats:boat")
+		pos.y=height+2
+		obj:set_pos(pos)
+	end
+	return true
+end
+)
---
+++ registerstuff.lua
@@ -0,0 +1,150 @@
+local floor = math.floor
+local ceil = math.ceil
+local min = math.min
+local max = math.max
+local random = math.random
+
+local mpath = minetest.get_modpath('islands')
+
+minetest.register_node("islands:dirt_with_grass_palm", {
+	description = "Dirt with Grass",
+	tiles = {"islands_grass.png", "default_dirt.png",
+		{name = "default_dirt.png^default_grass_side.png",
+			tileable_vertical = false}},
+	groups = {crumbly = 3, soil = 1, spreading_dirt_type = 1},
+	drop = 'default:dirt',
+	sounds = default.node_sound_dirt_defaults({
+		footstep = {name = "default_grass_footstep", gain = 0.25},
+	}),
+})
+
+minetest.register_node("islands:dirt_with_snow", {
+	description = "Dirt with Snow",
+	tiles = {"default_snow.png", "default_dirt.png",
+		{name = "default_dirt.png^default_snow_side.png",
+			tileable_vertical = false}},
+	groups = {crumbly = 3, soil = 1, spreading_dirt_type = 1, snowy = 1},
+	drop = 'default:dirt',
+	sounds = default.node_sound_dirt_defaults({
+		footstep = {name = "default_snow_footstep", gain = 0.2},
+	}),
+})
+
+minetest.register_decoration({
+	name = "islands:palm_tree",
+	deco_type = "schematic",
+	place_on = {"islands:dirt_with_grass_palm"},
+	sidelen = 20,
+	noise_params = {
+		offset = 0.006,
+		scale = 0.008,
+		spread = {x = 64, y = 64, z = 64},
+		seed = 2,
+		octaves = 2,
+		persist = 0.66
+	},
+	biomes = {"taiga",},
+	y_max = 21,
+	y_min = 3,
+	schematic = mpath .. "/schematics/schematik.mts",
+	flags = "place_center_x, place_center_z",
+})
+
+local function register_grass_decoration(offset, scale, length)
+	minetest.register_decoration({
+		name = "islands:grass_" .. length,
+		deco_type = "simple",
+		place_on = {"islands:dirt_with_grass_palm"},
+		sidelen = 16,
+		noise_params = {
+			offset = offset,
+			scale = scale,
+			spread = {x = 200, y = 200, z = 200},
+			seed = 100*length,
+			octaves = 3,
+			persist = 0.6
+		},
+		biomes = {"grassland"},
+		y_max = 31000,
+		y_min = 1,
+		decoration = "default:grass_" .. length,
+	})
+end
+
+register_grass_decoration(0.03,  0.09,  5)
+register_grass_decoration(0.015, 0.075, 4)
+register_grass_decoration(0,      0.06,  3)
+register_grass_decoration(-0.015,  0.045, 2)
+register_grass_decoration(-0.03,   0.03,  1)
+--[[
+minetest.register_on_newplayer(function(obj)
+	minetest.log('info','spawn stuff')
+	minetest.set_timeofday(0.35)
+	local inv = obj:get_inventory()
+	inv:add_item('main','binoculars:binoculars')
+	local pos = obj:get_pos()
+	local node = minetest.get_node(pos)
+	if node and not minetest.registered_nodes[node.name].walkable then
+		pos.y = 2
+		minetest.add_entity(pos,'boats:boat')
+		pos.y = 3
+		obj:set_pos(pos)
+	else
+		inv:add_item("main", "boats:boat")
+		for y = 2,38,2 do
+			pos.y=y
+			node = minetest.get_node(pos)
+			minetest.log('info',node.name)
+			if node and not minetest.registered_nodes[node.name].walkable then
+				obj:set_pos(pos)
+				break
+			end
+		end
+	end
+	return true
+end
+)--]]
+
+isln_pos1 = {x=0,y=0,z=0}
+isln_pos2 = {x=0,y=0,z=0}
+
+minetest.register_on_chat_message(
+	function(name, message)
+		if message == 'pos1' then
+			local plyr = minetest.get_player_by_name('singleplayer')
+			isln_pos1 = plyr:get_pos()
+			minetest.chat_send_all(dump(isln_pos1))
+		end
+	end
+)	
+minetest.register_on_chat_message(
+	function(name, message)
+		if message == 'pos2' then
+			local plyr = minetest.get_player_by_name('singleplayer')
+			isln_pos2 = plyr:get_pos()
+			minetest.chat_send_all(dump(isln_pos2))
+		end
+	end
+)	
+
+minetest.register_on_chat_message(
+	function(name, message)
+		if message == 'schm' then
+			local fname = minetest.get_worldpath() .."\\schematik.txt"
+			minetest.chat_send_all(fname)
+			minetest.create_schematic(isln_pos1,isln_pos2,nil,fname)
+			minetest.chat_send_all('saved')
+		end
+	end
+)	
+
+minetest.register_on_chat_message(
+	function(name, message)
+		if message == 'place' then
+			local fname = minetest.get_modpath('islands') .."\\schematics\\schematik.mts"
+			minetest.chat_send_all(fname)
+			minetest.place_schematic(isln_pos1,fname,'random',nil,false,'place_center_x,place_center_z')
+			minetest.chat_send_all('plced')
+		end
+	end
+)	
